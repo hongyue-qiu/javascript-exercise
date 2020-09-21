9bf6502c5075a86eef90f7e1515dafca61dfb588
hongyue-qiu <994059641@qq.com>
2020-09-21 15:37:45 +0800

    [qiuhongyue] finished the startWith

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,8 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let count = 0;
+  collection.forEach(function(e) {
+    if (e.startsWith('粤A')) count += 1;
+  });
+  return count;
 }
