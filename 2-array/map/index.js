df695397d1aab10e770d0379da080dc848ba979d
hongyue-qiu <994059641@qq.com>
2020-09-21 18:34:57 +0800

    [qiuhongyue] finished the map

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,8 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const array = [];
+  collection.forEach(function(e) {
+    array.push(String.fromCharCode(96 + e));
+  });
+  return array;
 }
