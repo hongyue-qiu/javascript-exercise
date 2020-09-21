1ce4ce03785e71b62b480788d5f4554e1eaf4d95
hongyue-qiu <994059641@qq.com>
2020-09-21 17:41:38 +0800

    [qiuhongyue] finished the find

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,8 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  let array;
+  collection.forEach(function(e) {
+    if (e.age <= 18 && e.age > 8) array = e.name;
+  });
+  return array;
 }
