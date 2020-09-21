8880a7a8690a23c52a48b6f9c8493405186c43ca
hongyue-qiu <994059641@qq.com>
2020-09-21 15:22:23 +0800

    [qiuhongyue] finished the templateString

--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -7,4 +7,8 @@ function getScore(id) {
 
 export default function getScoreOutput(person) {
   // TODO 8: 在这里写实现代码
+  const score = getScore(person.id);
+  const { firstName } = person;
+  const { lastName } = person;
+  return `你好，${lastName}${firstName}！你的考试成绩为${score}分`;
 }
