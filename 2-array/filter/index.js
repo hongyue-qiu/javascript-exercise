a814d965bd5907cae73e66d56ea93807dc4465ac
hongyue-qiu <994059641@qq.com>
2020-09-21 15:48:15 +0800

    [qiuhongyue] finished the 2-array filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,16 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const array = [];
+  collection.forEach(function(e) {
+    if (e % 3 == 0) array.push(e);
+  });
+  return array;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const array = [...new Set(collection)];
+  return array;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
