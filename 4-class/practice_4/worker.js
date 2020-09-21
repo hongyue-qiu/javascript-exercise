045b05a33c5bbdf80a0bcd12ce0f92b981aeb189
hongyue-qiu <994059641@qq.com>
2020-09-21 20:34:28 +0800

    [qiuhongyue] finished 4-class practice_4

--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,12 @@
 // TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  introduce() {
+    return `${this.basic_introduce()} I am a Worker. I have a job.`;
+  }
+}
