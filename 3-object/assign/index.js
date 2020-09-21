269f8ba3953cdd6946d22b01ba49d681ac335f82
hongyue-qiu <994059641@qq.com>
2020-09-21 18:41:51 +0800

    [qiuhongyue] finished 3-object assin

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,7 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const sourceNew = source;
+  sourceNew.serialNumber = '12345';
+  sourceNew.properties.status = 'processed';
+  return sourceNew;
 }
