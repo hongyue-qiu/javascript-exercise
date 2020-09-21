3683df29336ab11e89e35daa59a9abef0977f3ef
hongyue-qiu <994059641@qq.com>
2020-09-21 18:56:58 +0800

    [qiuhongyue] finished the 3-object valuse

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,10 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  var count = 0;
+  for (var i in source) {
+    count = parseInt(source[i]) + count;
+    console.log(parseInt(i));
+    console.log(i);
+  }
+  return count;
 }
