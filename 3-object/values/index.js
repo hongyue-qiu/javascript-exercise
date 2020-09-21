e46e66e71cbe0af13d74c18e1bdfce98ca9e69f3
hongyue-qiu <994059641@qq.com>
2020-09-21 19:38:37 +0800

    [qiuhongyue] finished 4-class practice_1

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,7 +1,7 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
-  var count = 0;
-  for (var i in source) {
+  let count = 0;
+  for (const i in source) {
     count = parseInt(source[i]) + count;
     console.log(parseInt(i));
     console.log(i);
